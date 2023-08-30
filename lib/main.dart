c4c5ef06cea5f9067e3f4070948f6e836a21c54f
Vokhanh12 <khanhyou2018@gmail.com>
2023-08-30 22:44:36 +0700

    add commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,34 @@ void main() {
 class MyGame extends FlameGame {
   SpriteComponent dog = SpriteComponent();
   SpriteComponent cat = SpriteComponent();
+  SpriteComponent background = SpriteComponent();
+  final double characterSize = 200;
   @override
   Future<void> onLoad() async {
     super.onLoad();
+    final screenWidth = size[0];
+    final screenHeigth = size[1];
+
     print('load game assets');
 
-    //load cat
+    add(background
+      ..sprite = await loadSprite('background.png')
+      ..size = size);
+
     cat
-      ..sprite = await loadSprite('image-cat.jpg')
-      ..size = Vector2(300, 300) // Đặt kích thước cho hình ảnh cat
-      ..x = 100 // Đặt vị trí x cho hình ảnh cat
-      ..y = 100; // Đặt vị trí y cho hình ảnh cat
+      ..sprite = await loadSprite('image-cat.png')
+      ..size = Vector2(
+          characterSize, characterSize) // Đặt kích thước cho hình ảnh cat
+      ..y = screenHeigth - characterSize; // Đặt vị trí y cho hình ảnh cat
 
     // Thêm các component vào trò chơi để hiển thị
     add(cat);
+
+    dog
+      ..sprite = await loadSprite('image-dog.png')
+      ..size = Vector2(characterSize, characterSize)
+      ..x = screenWidth - characterSize
+      ..y = screenHeigth - characterSize;
+    add(dog);
   }
 }
