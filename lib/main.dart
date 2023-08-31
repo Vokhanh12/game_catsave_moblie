1f0eb85ad28fd92e1cf1849e505dcd9dbfa72123
Vokhanh12 <khanhyou2018@gmail.com>
2023-08-31 20:56:19 +0700

    add commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,18 @@ class MyGame extends FlameGame {
   SpriteComponent dog = SpriteComponent();
   SpriteComponent cat = SpriteComponent();
   SpriteComponent background = SpriteComponent();
-  final double characterSize = 200;
+  SpriteComponent conversation_background = SpriteComponent();
+  bool _isShowConiversation = false;
+  final double characterSize = 220.0;
+  final double startScreen = 150;
+  final double conversationsSize = 100;
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
+
     final screenWidth = size[0];
     final screenHeigth = size[1];
-
     print('load game assets');
 
     add(background
@@ -27,7 +32,8 @@ class MyGame extends FlameGame {
       ..sprite = await loadSprite('image-cat.png')
       ..size = Vector2(
           characterSize, characterSize) // Đặt kích thước cho hình ảnh cat
-      ..y = screenHeigth - characterSize; // Đặt vị trí y cho hình ảnh cat
+      ..x = -startScreen
+      ..y = screenHeigth - characterSize - 10; // Đặt vị trí y cho hình ảnh cat
 
     // Thêm các component vào trò chơi để hiển thị
     add(cat);
@@ -35,8 +41,29 @@ class MyGame extends FlameGame {
     dog
       ..sprite = await loadSprite('image-dog.png')
       ..size = Vector2(characterSize, characterSize)
-      ..x = screenWidth - characterSize
+      ..x = screenWidth - characterSize + startScreen
       ..y = screenHeigth - characterSize;
     add(dog);
+
+    conversation_background
+      ..sprite = await loadSprite('blue-background.jpg')
+      ..size = Vector2(size[0], conversationsSize)
+      ..y = screenHeigth - conversationsSize;
+
+    if (_isShowConiversation) add(conversation_background);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    final screenWidth = size[0];
+    final screenHeigth = size[1];
+
+    if (dog.x != screenWidth - characterSize && cat.x != 0) {
+      dog.x -= 10;
+      cat.x += 10;
+    } else {
+      _isShowConiversation = true;
+    }
   }
 }
